fc0ac737a0bd44b165cea3cd92dfe703b75af262
Sayandeep <sdroy001@gmail.com>
2025-12-05 02:42:32 +0530

    created user profile

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,16 +5,29 @@ import { AuthProvider } from '@/contexts/authContext'
 
 const StackLayout = () => {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* Define your index/starting route */}
+      <Stack.Screen name="index" />
+      
+      {/* Define other routes if needed */}
+      {/* <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(main)" />
+       */}
+      {/* Configure the modal */}
+      <Stack.Screen
+        name="(main)/profileModal"
+        options={{ presentation: "modal" }}
+      />
+    </Stack>
   )
 }
+
 const RootLayout = () => {
   return(
     <AuthProvider>
       <StackLayout />
     </AuthProvider>
   )
-    
 }
 
 export default RootLayout
--- socket/socketEvents.ts
+++ socket/socketEvents.ts
@@ -19,3 +19,23 @@ export const testSocket = (payload: any, off: boolean = false) => {
     }
 
 }
+
+export const updateProfile = (payload: any, off: boolean = false) => {
+    const socket = getSocket();
+    if(!socket){
+        console.log("Socket is not connected");
+        return;
+    }
+
+    if(off){
+        // trun off listening to this event
+        socket.off("updateProfile", payload); // payload is the callback
+    }
+    else if(typeof payload === "function"){
+        socket.on("updateProfile", payload); // payload as callback for this event
+    }
+    else{
+        socket.emit("updateProfile", payload); // sending payload as data
+    }
+
+}
